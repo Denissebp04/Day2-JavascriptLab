ac9fe2bdadcc562efcaee8cee9138b63c9570283
Denisse Belliard <153006924+Denissebp04@users.noreply.github.com>
2024-09-20 12:11:21 -0400

    Practical Math/Problem solving, done

--- main.js
+++ main.js
@@ -58,3 +58,68 @@ console.log(ArithmeticChain)
 // Change isOver25
 const isOver26 = n1 + n2 + n3 + n4 > 26;
 console.log(isOver26)
+
+
+// Part II Practical Math
+
+
+// Info
+const dt = 1500 
+const V1 = 55
+const v2 = 60
+const v3 = 75
+const gal1 = 30
+const gal2 = 28
+const gal3 = 23 
+const budget = 175
+const galPrice = 3
+
+
+// Question 1: How many gallons of fuel will you need for the entire trip?
+
+// total gallon #1
+const totalGal1 = dt / gal1 ;
+console.log(totalGal1) 
+
+// total price need for gallon #1
+const totalPrice1 = totalGal1 * galPrice ;
+console.log(totalPrice1)
+
+// total gallon #2
+const totalGal2 = dt / gal2 ;
+console.log(totalGal2) 
+
+// total price need for gallon #2
+const totalPrice2 = totalGal2 * galPrice ;
+console.log(totalPrice2)
+
+// total gallon #3
+const totalGal3 = dt / gal3 ;
+console.log(totalGal3) 
+
+// total price need for gallon #3
+const totalPrice3 = totalGal3 * galPrice ;
+console.log(totalPrice3)
+
+// Question 2: Will your budget be enough to cover the fuel expense?
+
+// We need the total fuel budget needed for fuel not to exceed our actual fuel budget
+// which options gives us the most speed without exceeding the budget?
+const budgetUpdated = totalPrice1 <= budget && totalPrice2 <= budget && totalPrice3 <= budget; 
+console.log(budgetUpdated)
+const option1 = totalPrice1 <= budget ;
+console.log(option1)
+const option2 = totalPrice2 <= budget ;
+console.log(option2)
+const option3 = totalPrice3 <= budget ;
+console.log(option3)
+
+// Option1 and option2 are the only options within our budget
+// However between the two of them the fastest way seems to be option2 since its traveling at a higher speed
+
+// Question 3 : How long will the trip take, in hours?
+ const TotalTime = dt / v2 ;
+ console.log(TotalTime)
+
+    
+
